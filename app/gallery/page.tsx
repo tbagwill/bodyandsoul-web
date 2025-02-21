8d276850e802293b4a7d279e2115e6f2fcbb94de
tbagwill <tylerray.bagwill@calbaptist.edu>
2025-02-21 14:45:52 -0800

    Fix: Remove unused Image imports and clean up ESLint errors

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import Image from 'next/image';
 import Lightbox from 'yet-another-react-lightbox';
 import 'yet-another-react-lightbox/styles.css';
 import Placeholder from '../components/ui/Placeholder';
